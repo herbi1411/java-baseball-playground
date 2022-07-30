dc828213b0fcd99daf21be4bc82f830ada8501b5
YoonMinSoo <minsoo9615@naver.com>
2022-07-30 16:41:37 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,46 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    //요구사항 1
+    @Test
+    void part1_1(){
+        // "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1", "2");
+    }
+
+    @Test
+    void part1_2(){
+        // "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void part_2(){
+        String actual = "(1,2)";
+        String rt = actual.substring(1,4);
+        assertThat(rt).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt test")
+    void part_3(){
+        String actual = "abc";
+        assertThatThrownBy(() -> {
+            actual.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
     }
 }
